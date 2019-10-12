f95880ed09f8863bd6d75132f4ad0636909b6a34
Ankit Yadav (166241) <ankyadav@amdocs.com>
2019-10-12 15:10:19 +0530

    Thread Advance Learn

---
+++ src/mainPackae/ThreadPool.java
@@ -0,0 +1,108 @@
+package mainPackae;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ThreadPool implements Runnable,ThreadFactory,Callable<ThreadPool> {
+
+	Executors  e1;
+
+	public static ReentrantLock ren = new ReentrantLock();
+	
+	ThreadFactory th;
+	
+	ThreadPoolExecutor tll;
+	
+	//Difference b/w AtomicInteger and Volatile
+	
+	public void play() throws InterruptedException {
+		
+		ren.lock();
+		
+		boolean tryLockAcquired = ren.tryLock(5, TimeUnit.SECONDS);
+		try {
+			
+		}
+		// faltu code
+		finally {
+			ren.unlock();	
+		}
+		
+	}
+	
+	// overriding for ThreadFactory
+		@Override
+		public Thread newThread(Runnable r) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		// OverRiding for Callable
+		@Override
+		public ThreadPool call() throws Exception {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	@Override
+	public void run() {
+		
+		try {
+			play();
+			Thread.sleep(100);
+		}catch(InterruptedException e) {
+			System.err.println(e.toString());
+		}
+	}
+
+	public static void main(String... args) throws InterruptedException, ExecutionException {
+		ThreadPool t1 = new ThreadPool();
+		
+		Thread t = new Thread(new ThreadPool());
+		
+		Thread t2 = new Thread(new Runnable() {
+			
+			
+			@Override
+			public void run() {
+				
+			}
+		});
+		
+		
+		int x = Runtime.getRuntime().availableProcessors();
+		
+			ExecutorService rr = Executors.newFixedThreadPool(x);
+			
+		
+		Callable<ThreadPool> callable = new ThreadPool();
+		Callable<String> callable1 = new Callable<String>() {
+			
+			@Override
+			public String call() throws Exception {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			//Future<String>  future1 = rr.submit(callable1);
+			
+		};
+		
+		Future<ThreadPool>  future = rr.submit(callable);
+		
+		System.out.println(future.get());
+		System.out.println(x + " Number of cores in this system");
+	
+		//t.start();
+	}
+
+	
+	
+}
